401edc439e8251d598aca23ea84ef50b85c9e3bd
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-08-30 22:12:27 +0300

    fix: replace button position

--- custom-video-player/scripts/index.js
+++ custom-video-player/scripts/index.js
@@ -9,7 +9,8 @@ const volume = player.querySelector('.player__slider');
 const volumeButton = player.querySelector('.volume__button');
 const speedRate = player.querySelector('.play_speed');
 
-const wideScreenButton = player.querySelector('.');
+const wideScreenButton = player.querySelector('.widen');
+const fullScreenButton = player.querySelector('.fullscreen');
 
 const playButton = `<svg version="1.1" viewBox="0 0 512 512"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M405.2,232.9L126.8,67.2c-3.4-2-6.9-3.2-10.9-3.2c-10.9,0-19.8,9-19.8,20H96v344h0.1c0,11,8.9,20,19.8,20  c4.1,0,7.5-1.4,11.2-3.4l278.1-165.5c6.6-5.5,10.8-13.8,10.8-23.1C416,246.7,411.8,238.5,405.2,232.9z"/></svg>`;
 const pauseButton = `<svg version="1.1" viewBox="0 0 512 512"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M224,435.8V76.1c0-6.7-5.4-12.1-12.2-12.1h-71.6c-6.8,0-12.2,5.4-12.2,12.1v359.7c0,6.7,5.4,12.2,12.2,12.2h71.6   C218.6,448,224,442.6,224,435.8z"/><path d="M371.8,64h-71.6c-6.7,0-12.2,5.4-12.2,12.1v359.7c0,6.7,5.4,12.2,12.2,12.2h71.6c6.7,0,12.2-5.4,12.2-12.2V76.1   C384,69.4,378.6,64,371.8,64z"/></g></svg>`;
@@ -65,22 +66,22 @@ function muteHandler() {
 }
 
 function wideScreenHandler() {
-  if (this.classList.contains('unmuted')) {
-    this.classList.remove('unmuted');
-    this.innerHTML = volumeMute;
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    this.innerHTML = widenScreen;
   } else {
-    this.classList.add('unmuted');
-    this.innerHTML = volumeNormal;
+    this.classList.add('active');
+    this.innerHTML = shrinkScreen;
   }
 }
 
 function fullScreenHandler() {
-  if (this.classList.contains('unmuted')) {
-    this.classList.remove('unmuted');
-    this.innerHTML = volumeMute;
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+    this.innerHTML = fullScreen;
   } else {
-    this.classList.add('unmuted');
-    this.innerHTML = volumeNormal;
+    this.classList.add('active');
+    this.innerHTML = normalScreen;
   }
 }
 
@@ -102,6 +103,8 @@ skipButtons.forEach(element => {
   element.addEventListener('click', skip);
 });
 volumeButton.addEventListener('click', muteHandler);
+wideScreenButton.addEventListener('click', wideScreenHandler);
+fullScreenButton.addEventListener('click', fullScreenHandler);
 
 let progressMousedown = false;
 let volumeMousedown = false;
