dcf806d1068d14645229c97fc6c755aa8fc34ff1
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-08-30 20:44:24 +0300

    feat: add basic functionality

---
+++ custom-video-player/scripts/variables.js
@@ -0,0 +1,15 @@
+// export const player = document.querySelector('.player');
+// export const video = player.querySelector('.viewer');
+// export const progress = player.querySelector('.progress');
+// export const progressBar = player.querySelector('.progress__filled');
+// export const toggle = player.querySelector('.toggle');
+// export const skipButtons = player.querySelector('[data-skip]');
+// export const ranges = player.querySelector('.player__slider');
+
+// export const routePlay = './assets/icons/play_icon.svg';
+// export const routePause = './assets/icons/pause_icon.svg';
+// export const routeStop = './assets/icons/stop_icon.svg';
+// export const routeForward = './assets/icons/forward_icon.svg';
+// export const routeBackward = './assets/icons/backward_icon.svg';
+// export const routeMute = './assets/icons/mute_volume_icon.svg';
+// export const routeUnmute = './assets/icons/volume_icon.svg';
